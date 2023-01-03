efef7848f30cf7b45979dcca32261defd5932003
Pisarev Danila <jobdan.dev@gmail.com>
2023-01-03 14:31:06 +0300

    fix: change memoization bugs in the login and registration forms

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
 import { Form } from "components/Form";
 import { useAppDispatch, useAppSelector } from "hooks";
-
 import { loginThunk } from "store/thunks/login.thunk";
 import { Input } from "components/Input";
+
+enum AvailableLoginFormFieldsId {
+  EMAIL = "email",
+  PASSWORD = "password",
+}
 
 export const LoginForm = () => {
   const [email, setEmail] = React.useState<string>("");
@@ -14,32 +17,30 @@ export const LoginForm = () => {
   const navigate = useNavigate();
   const loading = useAppSelector((state) => state.user.loading);
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> =
-    React.useCallback((e) => {
-      e.preventDefault();
-      dispatch(loginThunk({ email, password }))
-        .unwrap()
-        .then(() => {
-          navigate("/");
-        })
-        .catch(console.error);
-    }, []);
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
 
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> =
-    React.useCallback((e) => {
-      if (e.target.name === "email") {
-        setEmail(e.target.value);
-      } else {
-        setPassword(e.target.value);
-      }
-    }, []);
+    dispatch(loginThunk({ email, password }))
+      .unwrap()
+      .then(() => {
+        navigate("/");
+      })
+      .catch(console.error);
+  };
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.id === AvailableLoginFormFieldsId.EMAIL) {
+      setEmail(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit} loading={loading} buttonLabel="Login">
       <Input
         type="email"
-        name="email"
-        id="email"
+        id={AvailableLoginFormFieldsId.EMAIL}
         label="E-mail"
         value={email}
         onChange={handleChange}
@@ -48,8 +49,7 @@ export const LoginForm = () => {
       />
       <Input
         type="password"
-        id="password"
-        name="password"
+        id={AvailableLoginFormFieldsId.PASSWORD}
         label="Password"
         value={password}
         onChange={handleChange}
--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import { Form } from "components/Form";
-
-import { useAppDispatch, useAppSelector } from "hooks";
-import { registerThunk } from "store/thunks/register.thunk";
-
-import { Input } from "components/Input";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { registerThunk } from "../../store/thunks/register.thunk";
+import { Input } from "../../components/Input";
+import { Form } from "../../components/Form";
 
 enum AvailableRegistrationFormFieldsId {
   NAME = "name",
@@ -14,36 +12,34 @@ enum AvailableRegistrationFormFieldsId {
   PASSWORD = "password",
 }
 
-export const RegistrationForm = () => {
+export const RegistrationForm: React.FC = () => {
   const [email, setEmail] = React.useState<string>("");
   const [password, setPassword] = React.useState<string>("");
   const [name, setName] = React.useState<string>("");
 
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const { loading, error } = useAppSelector((state) => state.user);
+  const { loading } = useAppSelector((state) => state.user);
 
-  const handleSubmit: React.FormEventHandler<HTMLFormElement> =
-    React.useCallback((e) => {
-      e.preventDefault();
-      dispatch(registerThunk({ email, password, name }))
-        .unwrap()
-        .then(() => {
-          navigate("/");
-        })
-        .catch(console.error);
-    }, []);
+  const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    dispatch(registerThunk({ email, password, name }))
+      .unwrap()
+      .then(() => {
+        navigate("/");
+      })
+      .catch(console.error);
+  };
 
-  const handleChange: React.ChangeEventHandler<HTMLInputElement> =
-    React.useCallback((e) => {
-      if (e.target.id === AvailableRegistrationFormFieldsId.EMAIL) {
-        setEmail(e.target.value);
-      } else if (e.target.id === AvailableRegistrationFormFieldsId.NAME) {
-        setName(e.target.value);
-      } else {
-        setPassword(e.target.value);
-      }
-    }, []);
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    if (e.target.id === AvailableRegistrationFormFieldsId.EMAIL) {
+      setEmail(e.target.value);
+    } else if (e.target.id === AvailableRegistrationFormFieldsId.NAME) {
+      setName(e.target.value);
+    } else {
+      setPassword(e.target.value);
+    }
+  };
 
   return (
     <Form onSubmit={handleSubmit} buttonLabel="Sign up" loading={loading}>
